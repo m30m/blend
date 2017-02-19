6666d04b0ca573022c3869c0baf03eb936af33eb
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2017-02-20 00:09:20 +0330

    Correct handling of exceptions for .res file

--- Parser.java
+++ Parser.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Stack;
 
@@ -35,70 +36,61 @@ public class Parser {
         return scanner.lineNumber(); // Or any other name you used in your Scanner
     }
 
-    public void Parse() {
-        try {
-            int tokenId = nextTokenID();
-            int curNode = 0;
-            boolean notAccepted = true;
-            while (notAccepted) {
-                String token = symbols[tokenId];
-                PTBlock ptb = parseTable[curNode][tokenId];
-                switch (ptb.getAct()) {
-                    case PTBlock.ActionType.Error: {
+    public void Parse() throws Exception {
+        int tokenId = nextTokenID();
+        int curNode = 0;
+        boolean notAccepted = true;
+        while (notAccepted) {
+            String token = symbols[tokenId];
+            PTBlock ptb = parseTable[curNode][tokenId];
+            switch (ptb.getAct()) {
+                case PTBlock.ActionType.Error: {
+                    throw new Exception(String.format("Compile Error (" + token + ") at line " + scanner.lineNumber() + " @ " + curNode));
+                }
+                case PTBlock.ActionType.Shift: {
+                    cg.Generate(ptb.getSem(), currentToken);
+                    tokenId = nextTokenID();
+                    curNode = ptb.getIndex();
+                }
+                break;
+
+                case PTBlock.ActionType.PushGoto: {
+                    parseStack.push(curNode);
+                    curNode = ptb.getIndex();
+                }
+                break;
+
+                case PTBlock.ActionType.Reduce: {
+                    if (parseStack.size() == 0) {
                         throw new Exception(String.format("Compile Error (" + token + ") at line " + scanner.lineNumber() + " @ " + curNode));
                     }
-                    case PTBlock.ActionType.Shift: {
-                        cg.Generate(ptb.getSem(), currentToken);
-                        tokenId = nextTokenID();
-                        curNode = ptb.getIndex();
-                    }
-                    break;
 
-                    case PTBlock.ActionType.PushGoto: {
-                        parseStack.push(curNode);
-                        curNode = ptb.getIndex();
-                    }
-                    break;
-
-                    case PTBlock.ActionType.Reduce: {
-                        if (parseStack.size() == 0) {
-                            throw new Exception(String.format("Compile Error (" + token + ") at line " + scanner.lineNumber() + " @ " + curNode));
-                        }
-
-                        curNode = parseStack.pop();
-                        ptb = parseTable[curNode][ptb.getIndex()];
-                        cg.Generate(ptb.getSem(), currentToken);
-                        curNode = ptb.getIndex();
-                    }
-                    break;
-
-                    case PTBlock.ActionType.Accept: {
-                        notAccepted = false;
-                    }
-                    break;
-
+                    curNode = parseStack.pop();
+                    ptb = parseTable[curNode][ptb.getIndex()];
+                    cg.Generate(ptb.getSem(), currentToken);
+                    curNode = ptb.getIndex();
                 }
+                break;
+
+                case PTBlock.ActionType.Accept: {
+                    notAccepted = false;
+                }
+                break;
+
             }
-            cg.FinishCode();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
         }
+        cg.FinishCode();
+
     }
 
-    int nextTokenID() {
-        try {
+    int nextTokenID() throws Exception {
             currentToken = scanner.NextToken();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         int i;
         for (i = 0; i < symbols.length; i++) {
             if (symbols[i].equals(currentToken.parser_token))
                 return i;
         }
-        (new Exception("Undefined token: " + currentToken.parser_token)).printStackTrace();
-        return 0;
+        throw (new Exception("Undefined token: " + currentToken.parser_token));
     }
 
     public void WriteOutput(String outputFile) // You can change this function, if you think it is not comfortable
